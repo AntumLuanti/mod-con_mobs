30f77b3584c1bf1e0ff2d07c0994260773996b86
Jordan Irwin <antumdeluge@gmail.com>
2026-06-24 00:41:48 -0700

    Parse api priority from con_mobs.api_priority setting

--- init.lua
+++ init.lua
@@ -5,6 +5,11 @@ con_mobs = {
 con_mobs.modpath = core.get_modpath(con_mobs.modname)
 
 
+local priority = core.settings:get("con_mobs.api_priority")
+local priority_list = {}
+for api_name in string.gmatch(priority, "([^,]+)") do
+	table.insert(priority_list, string.gsub(api_name, "^%s*(.-)%s*$", "%1"))
+end
 if con_mobs.register == nil then
 	core.log("error", "no mobs API detected")
 	return
