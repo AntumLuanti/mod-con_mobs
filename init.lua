d75b1d3581fc08a54d2e5f0b634269669896c759
Jordan Irwin <antumdeluge@gmail.com>
2026-06-24 00:40:28 -0700

    Log error if con_mobs.register not defined

--- init.lua
+++ init.lua
@@ -3,3 +3,9 @@ con_mobs = {
 	modname = core.get_current_modname()
 }
 con_mobs.modpath = core.get_modpath(con_mobs.modname)
+
+
+if con_mobs.register == nil then
+	core.log("error", "no mobs API detected")
+	return
+end
